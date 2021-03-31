1432d1dc27b2bd6063b4d203d15cde6ef58a1782
thomas-dsv <thomas.de-sousa-vieira@etu.unice.fr>
2021-03-31 15:49:45 +0200

    Initial commit

---
+++ app/src/main/java/com/dsv/td1/si3_ihm_aliments/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.dsv.td1.si3_ihm_aliments.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
