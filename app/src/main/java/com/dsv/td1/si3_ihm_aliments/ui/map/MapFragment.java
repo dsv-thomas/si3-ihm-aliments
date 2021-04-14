e8cf8cd7f7d1f01726838237624ce618e9cc287a
Laurie-Fernandez <59823387+Laurie-Fernandez@users.noreply.github.com>
2021-04-14 12:17:57 +0200

    OSM fonctionnel #1

--- app/src/main/java/com/dsv/td1/si3_ihm_aliments/ui/map/MapFragment.java
+++ app/src/main/java/com/dsv/td1/si3_ihm_aliments/ui/map/MapFragment.java
@@ -1,5 +1,6 @@
 package com.dsv.td1.si3_ihm_aliments.ui.map;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,16 +12,30 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 
 import com.dsv.td1.si3_ihm_aliments.R;
 
-public class MapFragment extends Fragment {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
 
+import java.util.ArrayList;
+
+
+public class MapFragment extends Fragment {
+    private MapView map;
     private MapViewModel mapViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_map, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
         mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -29,6 +44,75 @@ public class MapFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        IMapController mapController;
+        ItemizedOverlayWithFocus<OverlayItem> mMyLocationOverlay;
+
+        assert getActivity()!=null;
+        map = root.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);    //render
+        map.setBuiltInZoomControls(true);               // zoomable
+        map.setMultiTouchControls(true);
+        mapController = map.getController();
+        mapController.setZoom(18.0);
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+        mapController.setCenter(startPoint);
+
+        //create a new item to draw on the map
+        //your items
+        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        OverlayItem home = new OverlayItem("F. Rallo", "nos bureaux", new GeoPoint(43.65020,7.00517));
+        Drawable m = home.getMarker(0);
+
+        items.add(home); // Lat/Lon decimal degrees
+        items.add(new OverlayItem("Resto", "chez babar", new GeoPoint(43.64950,7.00517))); // Lat/Lon decimal degrees
+
+        //the Place icons on the map with a click listener
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getActivity().getApplicationContext(), items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        return false;
+                    }
+                });
+
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+        /////// FIN AFFICHAGE MAP
+
         return root;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        assert getActivity() != null;
+        Configuration.getInstance().load(getActivity().getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()));
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 }
