6eaf5e5d68786e96eeb646f83cba0f9f50bba83b
Laurie-Fernandez <59823387+Laurie-Fernandez@users.noreply.github.com>
2021-04-14 15:45:30 +0200

    Différenciation producteur/client sur le menu #2

--- app/src/main/java/com/dsv/td1/si3_ihm_aliments/MainActivity.java
+++ app/src/main/java/com/dsv/td1/si3_ihm_aliments/MainActivity.java
@@ -4,13 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.dsv.td1.si3_ihm_aliments.controller.ControllerActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        findViewById(R.id.start).setOnClickListener(click-> {
+        findViewById(R.id.start_client).setOnClickListener(click-> {
             Intent intent = new Intent(getApplicationContext(),ControllerActivity.class);
             startActivity(intent);
         });
---
+++ app/src/main/java/com/dsv/td1/si3_ihm_aliments/ui/profile/ProfileDescriptionFragment.java
@@ -0,0 +1,8 @@
+package com.dsv.td1.si3_ihm_aliments.ui.profile;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileDescriptionFragment  extends Fragment {
+
+
+}
